fe1ffdb800371b2494dc7fc9f618e12706471458
IanMutuma <ian97mutuma@gmail.com>
2022-06-08 22:36:11 +0300

    commit tree cf72b6dd2d5086f77f8aa8564fb70cac6d51f622

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function saturdayFun (name = "roller-skate"){
+    return `This Saturday, I want to ${name}!`;
+}
+saturdayFun();
+saturdayFun("football");
+
+function mondayWork (toDo= "go to the office"){
+    return `This Monday, Iwill ${toDo}.`
+    mondayWork ("work half day");
+}
